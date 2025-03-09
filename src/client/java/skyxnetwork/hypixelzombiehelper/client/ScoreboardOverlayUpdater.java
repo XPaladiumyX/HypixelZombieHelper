4eb12b4a4d49ca1093bb0bf8426b291f735563c9
XPaladiumyX <allegra.yann@gmail.com>
2025-03-09 15:44:37 +0100

    Update in live

---
+++ src/client/java/skyxnetwork/hypixelzombiehelper/client/ScoreboardOverlayUpdater.java
@@ -0,0 +1,15 @@
+package skyxnetwork.hypixelzombiehelper.client;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+public class ScoreboardOverlayUpdater {
+
+    public static void register() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null) {
+                // Mettre à jour les données de l'overlay
+                ScoreboardOverlay.updateData();
+            }
+        });
+    }
+}
